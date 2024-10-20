d91adaa2bd0abd8c39889624d81a211a04e50e19
JoshGibbsSimplyStream <mrpurple6411@gmail.com>
2024-10-19 18:59:39 -0600

    Rework debug calls

--- Bepinex-TwitchController/TwitchEventManager.cs
+++ Bepinex-TwitchController/TwitchEventManager.cs
@@ -28,169 +28,170 @@
                 return;
             }
 
-            if (authorized)
+            if (!authorized)
             {
-                if (triggerLower == "!allevents")
+                return;
+            }
+
+            if (triggerLower == "!allevents")
+            {
+                controller.eventLookup.SendAllEvents();
+                return;
+            }
+
+            if (e.TriggerText.Contains("WE DID IT! WE HIT A LEVEL 5 HYPE TRAIN!"))
+            {
+                controller.eventLookup.Lookup("HypeTrainLevel5Complete", "HypeTrain 5 Complete!!!", e);
+                return;
+            }
+
+            Regex regex = new Regex(Controller._secrets.regex);
+            Match match = regex.Match(e.TriggerText);
+            Regex not_num_period = new Regex("[^0-9.]");
+
+
+            if (match.Success)
+            {
+                string user = match.Groups["user"].Value;
+                string donation = not_num_period.Replace(match.Groups["donation"].Value, "");
+
+                if (string.IsNullOrWhiteSpace(donation) || !float.TryParse(donation, out float donated))
                 {
-                    controller.eventLookup.SendAllEvents();
+                    Console.WriteLine($"Parsing tip as float failed for {user} and amount of {donation}");
                     return;
                 }
 
-                if (e.TriggerText.Contains("WE DID IT! WE HIT A LEVEL 5 HYPE TRAIN!"))
+                int tip = (int)(donated * 100);
+
+                Console.WriteLine($"User:{user},  Tip:{tip}");
+                controller.eventLookup.Lookup(tip, user, e);
+                return;
+            }
+
+            if (e.TriggerText.StartsWith("!"))
+            {
+                string[] x = e.TriggerText.Trim().Split('/');
+
+                switch (x.Length)
                 {
-                    controller.eventLookup.Lookup("HypeTrainLevel5Complete", "HypeTrain 5 Complete!!!", e);
-                    return;
-                }
-
-                Regex regex = new Regex(Controller._secrets.regex);
-                Match match = regex.Match(e.TriggerText);
-                Regex not_num_period = new Regex("[^0-9.]");
-
-
-                if (match.Success)
-                {
-                    string user = match.Groups["user"].Value;
-                    string donation = not_num_period.Replace(match.Groups["donation"].Value, "");
-
-                    if (!float.TryParse(donation, out float donated))
-                    {
-                        Console.WriteLine($"Parsing tip as float failed for {user} and amount of {donation}");
-                        return;
-                    }
-
-                    int tip = (int)(donated * 100);
-
-                    Console.WriteLine($"User:{user},  Tip:{tip}");
-                    controller.eventLookup.Lookup(tip, user, e);
-                }
-
-                if (e.TriggerText.StartsWith("!"))
-                {
-                    string[] x = e.TriggerText.Split('/');
-
-                    switch (x.Length)
-                    {
-                        case 1:
+                    case 1:
+                        {
+                            // if first letter after the ! is e, it's a named event and if it's b, it's bits
+                            string type = triggerLower.Trim().Substring(1, 1);
+                            string trigger = e.TriggerText.Trim().Substring(2);
+                            if (string.IsNullOrWhiteSpace(trigger))
                             {
-
-                                // if first letter after the ! is e, it's a named event and if it's b, it's bits
-                                string type = triggerLower.Substring(1, 2);
-
-                                if (type == "e")
-                                {
-                                    string trigger = e.TriggerText.Substring(3).Trim();
-                                    if (string.IsNullOrWhiteSpace(trigger))
-                                    {
-                                        // trigger !e is invalid as it needs an event name
-                                        Console.WriteLine($"[Error] Trigger {e.TriggerText} is invalid as it needs an event name eg !ePrime");
-                                    }
-
-                                    if (controller.eventLookup.Contains(trigger))
-                                    {
-                                        Console.WriteLine($"User:{streamer},  Trigger:{trigger}");
-                                        controller.eventLookup.Lookup(trigger, username, e);
-                                        return;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"{trigger} Not Found");
-                                    }
-                                }
-                                else if (type == "b")
-                                {
-                                    string trigger = e.TriggerText.Substring(3);
-                                    if (int.TryParse(trigger, out int bits))
-                                    {
-                                        Console.WriteLine($"User:{username},  Bits:{bits}");
-                                        controller.eventLookup.Lookup(bits, username, e);
-                                        return;
-                                    }
-                                }
-                                else if (type == "$")
-                                {
-                                    string trigger = e.TriggerText.Substring(3);
-                                    if (float.TryParse(trigger, out float donated))
-                                    {
-                                        int tip = (int)(donated * 100);
-                                        Console.WriteLine($"User:{username},  Tip:{tip}");
-                                        controller.eventLookup.Lookup(tip, username, e);
-                                        return;
-                                    }
-                                }
-                                break;
+                                // trigger !e is invalid as it needs an event name
+                                Console.WriteLine($"[Error] Trigger {e.TriggerText.Trim()} is invalid as it needs an event name eg !e1000, !b1000, !ePrime, !e1000, !$5.00");
                             }
-                        case 2:
+
+                            switch (type)
                             {
-                                int bits;
-                                string user = x[0].Substring(1);
-                                string trigger = x[1].Trim();
-                                if (!int.TryParse(trigger, out bits))
-                                {
-                                    if (controller.eventLookup.Contains(trigger))
-                                    {
-                                        Console.WriteLine($"User:{user},  Trigger:{trigger}");
-                                        controller.eventLookup.Lookup(trigger, user, e);
-                                        return;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"{trigger} Not Found");
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"User:{user},  Bits:{bits}");
-                                    controller.eventLookup.Lookup(bits, user, e);
+                                case "e" when controller.eventLookup.Contains(trigger):
+                                    Console.WriteLine($"User:{streamer},  Trigger:{trigger}");
+                                    controller.eventLookup.Lookup(trigger, username, e);
                                     return;
-                                }
-
-                                break;
+                                case "b" when int.TryParse(trigger, out int bits):
+                                    Console.WriteLine($"User:{username},  Bits:{bits}");
+                                    controller.eventLookup.Lookup(bits, username, e);
+                                    return;
+                                case "$" when (float.TryParse(trigger, out float donated)):
+                                    int tip = (int)(donated * 100);
+                                    Console.WriteLine($"User:{username},  Tip:{tip}");
+                                    controller.eventLookup.Lookup(tip, username, e);
+                                    return;
+                                default:
+                                    Console.WriteLine($"[Error] Trigger {e.TriggerText.Trim()} is invalid.");
+                                    break;
                             }
-                        case 3:
-                            {
-                                string user = x[0].Substring(1);
-                                string type = x[1].Trim().ToLowerInvariant();
-                                string trigger = x[2].Trim();
+                            break;
+                        }
+                    case 2:
+                        {
+                            string user = x[0].Substring(1);
+                            string trigger = x[1].Trim();
 
-                                if ((type == "event" || type == "e"))
+                            if (string.IsNullOrWhiteSpace(trigger))
+                            {
+                                // needs something after the / eg !MrPurple6411/Prime or !MrPurple6411/100
+                                Console.WriteLine($"[Error] Trigger {e.TriggerText.Trim()} is invalid as it needs something after the / eg !MrPurple6411/Prime or !MrPurple6411/100");
+                            }
+
+                            if (!int.TryParse(trigger, out int bits))
+                            {
+                                if (controller.eventLookup.Contains(trigger))
                                 {
-                                    if (controller.eventLookup.Contains(trigger))
-                                    {
-                                        Console.WriteLine($"User:{user},  Trigger:{trigger}");
-                                        controller.eventLookup.Lookup(trigger, user, e);
-                                        return;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"{trigger} Not Found");
-                                    }
-                                }
-                                else if ((type == "bits" || type == "b"))
-                                {
-                                    int bits;
-                                    if (!int.TryParse(trigger, out bits))
-                                    {
-                                        Console.WriteLine($"failed to parse {trigger} in {e.TriggerText} to a bits value.");
-                                        return;
-                                    }
-                                    Console.WriteLine($"User:{user},  Bits:{bits}");
-                                    controller.eventLookup.Lookup(bits, user, e);
+                                    Console.WriteLine($"User:{user},  Length: {x.Length},  Trigger:{trigger}");
+                                    controller.eventLookup.Lookup(trigger, user, e);
                                     return;
                                 }
                                 else
                                 {
-                                    Console.WriteLine($"Invalid type {type} in {e.TriggerText}. Must be 'event', 'e', 'bits' or 'b'");
-                                    return;
+                                    Console.WriteLine($"{trigger} Not Found");
                                 }
-
-                                break;
                             }
-                        default:
+                            else
                             {
-                                Console.WriteLine($"Invalid number of arguments in {e.TriggerText}");
+                                Console.WriteLine($"User:{user},  Length: {x.Length},  Bits:{bits}");
+                                controller.eventLookup.Lookup(bits, user, e);
                                 return;
                             }
-                    }
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            string user = x[0].Substring(1);
+                            string type = x[1].Trim().ToLowerInvariant();
+                            string trigger = x[2].Trim();
+
+                            if (string.IsNullOrWhiteSpace(trigger))
+                            {
+                                // needs something after the / eg !MrPurple6411/event/Prime or !MrPurple6411/bits/100
+                                Console.WriteLine($"[Error] Trigger {e.TriggerText.Trim()} is invalid as it needs something after the / eg !MrPurple6411/Prime or !MrPurple6411/100");
+                            }
+
+                            switch (type)
+                            {
+                                case "event":
+                                case "e":
+                                    if (controller.eventLookup.Contains(trigger))
+                                    {
+                                        Console.WriteLine($"User:{user},  Length: {x.Length},  Trigger:{trigger}");
+                                        controller.eventLookup.Lookup(trigger, user, e);
+                                        return;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"{trigger} Not Found");
+                                    }
+                                    break;
+                                case "bits":
+                                case "b":
+                                    {
+                                        int bits;
+                                        if (!int.TryParse(trigger, out bits))
+                                        {
+                                            Console.WriteLine($"failed to parse {trigger} in {e.TriggerText} to a bits value.");
+                                            return;
+                                        }
+                                        Console.WriteLine($"User:{user},  Length: {x.Length},  Bits:{bits}");
+                                        controller.eventLookup.Lookup(bits, user, e);
+                                        return;
+                                    }
+
+                                default:
+                                    Console.WriteLine($"Invalid type {type} in {e.TriggerText}. Must be 'event', 'e', 'bits' or 'b'");
+                                    return;
+                            }
+
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"Invalid number of arguments in {e.TriggerText}");
+                            return;
+                        }
                 }
             }
 
